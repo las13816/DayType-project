9d102299a26d620997fef8a674f0b4b65513acad
las13816 <las13816@email.vccs.edu>
2024-04-09 11:56:17 -0400

    Getting ready for new client program

--- main.cpp
+++ main.cpp
@@ -4,44 +4,13 @@
 #include "dayType.h" 
 
 using namespace std;
+// unit test 
+#include <iostream>
+#include <string>
+#include "dateType.h"
+using namespace std;
 
-// Main function 
-// Test class 
 int main()
 {
 
-	dayType day;
-
-
-	day.printTo();
-
-	string inputDay = "Monday";
-
-	dayType daytwo(inputDay);
-	daytwo.printTo();
-
-
-	// Add 3 days to current day 
-	day.printTo();
-	int add1 = 3;
-	string out1;
-	out1 = day.addToDay(inputDay, add1);
-	cout << "After adding 3 days:" << out1 << endl;
-
-	int add2 = 30;
-	string out2;
-
-	// Add 30 days to current day 
-	out2 = day.addToDay(inputDay, add2);
-	cout << "After adding 30 days:" << out2 << endl;
-	int add3 = 365;
-	string out3;
-
-	// Add 365 days to current day 
-	out3 = day.addToDay(inputDay, add3);
-	cout << "After adding 365 days:" << out3 << endl;
-
-
-
-
-}
+};
