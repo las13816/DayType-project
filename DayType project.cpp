8b359f08cbf7a0876548446b4c5b21aeb33f2e1c
las13816 <las13816@email.vccs.edu>
2024-04-13 22:31:11 -0400

    Implemented feedback from submission comment

--- DayType project.cpp
+++ DayType project.cpp
@@ -1,192 +1,68 @@
+// dayType.cpp
+
 #include <iostream>
+#include <string>
 #include "dayType.h"
 
-// Default constructor
 using namespace std;
 
+string dayType::weekDays[7] = { "Sunday", "Monday", "Tuesday",
+                               "Wednesday", "Thursday", "Friday",
+                               "Saturday" };
+
+void dayType::print() const {
+    cout << day;
+}
+
+string dayType::nextDay() const {
+    for (int i = 0; i < 7; i++) {
+        if (weekDays[i] == day) {
+            return weekDays[(i + 1) % 7];
+        }
+    }
+    return ""; // Handle case where day is not found
+}
+
+string dayType::prevDay() const {
+    for (int i = 0; i < 7; i++) {
+        if (weekDays[i] == day) {
+            return weekDays[(i + 6) % 7]; // Modulo 7 to handle wrap around
+        }
+    }
+    return ""; // Handle case where day is not found
+}
+
+void dayType::addDay(int nDays) {
+    for (int i = 0; i < 7; i++) {
+        if (weekDays[i] == day) {
+            day = weekDays[(i + nDays) % 7];
+            break;
+        }
+    }
+}
+
+void dayType::setDay(string d) {
+    bool found = false;
+    for (int i = 0; i < 7; i++) {
+        if (weekDays[i] == d) {
+            day = d;
+            found = true;
+            break;
+        }
+    }
+    if (!found) {
+        day = "Sunday"; // Assign default value if input doesn't match
+    }
+}
+
+string dayType::getDay() const {
+    return day;
+}
+
 dayType::dayType() {
+    day = "Sunday"; // Initialize to default value
+}
 
-    days[0] = "Sunday";
-    days[1] = "Monday";
-    days[2] = "Tuesday";
-    days[3] = "Wednesday";
-    days[4] = "Thursday";
-    days[5] = "Friday";
-    days[6] = "Saturday";
-
-    today = days[0];
-    tomorrow = days[0 + 1];
-    previousDay = days[0 + 6];
-    cout << "From default constructor ";
-};
-
-
-dayType::~dayType()
-{
-};
-
-// Assign days 
-dayType::dayType(string inputDay)
-{
-    days[0] = "Sunday";
-    days[1] = "Monday";
-    days[2] = "Tuesday";
-    days[3] = "Wednesday";
-    days[4] = "Thursday";
-    days[5] = "Friday";
-    days[6] = "Saturday";
-
-    today = inputDay;
-
-
-    // Assign tomorrow and yesterday
-    tomorrow = dayType::setReturnNext(today);
-    previousDay = dayType::setReturnPrevious(today);
-
-
-
-    // Print statement 
-    cout << "From constructor with parameter ";
-
-};
-
-// Assign values to tomorrow 
-string dayType::setReturnNext(string inputDay)
-{
-    if (today == days[0])
-    {
-        tomorrow = days[1];
-    }
-    if (today == days[1])
-    {
-        tomorrow = days[2];
-    }
-    if (today == days[2])
-    {
-        tomorrow = days[3];
-    }
-    if (today == days[3])
-    {
-        tomorrow = days[4];
-    }
-    if (today == days[4])
-    {
-        tomorrow = days[5];
-    }
-    if (today == days[5])
-    {
-        tomorrow = days[6];
-    }
-    if (today == days[6])
-    {
-        tomorrow = days[0];
-    }
-    return tomorrow;
-
-};
-
-// Assign values to previous day or yesterday
-string dayType::setReturnPrevious(string inputDay)
-{
-    if (today == days[0])
-    {
-        previousDay = days[6];
-    }
-    if (today == days[1])
-    {
-        previousDay = days[0];
-    }
-    if (today == days[2])
-    {
-        previousDay = days[1];
-    }
-    if (today == days[3])
-    {
-        previousDay = days[2];
-    }
-    if (today == days[4])
-    {
-        previousDay = days[3];
-    }
-    if (today == days[5])
-    {
-        previousDay = days[4];
-    }
-    if (today == days[6])
-    {
-        previousDay = days[5];
-    }
-    return previousDay;
-
-};
-
-// Print 
-void dayType::printDay(string today)
-{
-    cout << "Today is: " << today << endl;
-};
-
-// Set the day and return 
-string dayType::setDay(string inputDay)
-{
-    today = inputDay;
-    return 0;
-};
-
-// Return value for current day 
-string dayType::returnDay()
-{
-    return today;
-};
-
-// Loop for addign days to current day 
-string dayType::addToDay(string today, int addto)
-{
-
-    int index;
-
-    if (today == days[0])
-    {
-        index = 0;
-    }
-    if (today == days[1])
-    {
-        index = 1;
-    }
-    if (today == days[2])
-    {
-        index = 2;
-    }
-    if (today == days[3])
-    {
-        index = 3;
-    }
-    if (today == days[4])
-    {
-        index = 4;
-    }
-    if (today == days[5])
-    {
-        index = 5;
-    }
-    if (today == days[6])
-    {
-        index = 6;
-    }
-
-    int z = (addto + index) % 7;
-    string addedDay = days[z];
-    return addedDay;
-
-};
-
-
-
-
-// Print statements 
-void dayType::printTo()
-{
-    cout << "Today is: " << today << endl;
-    cout << "Tomorrow is: " << tomorrow << endl;
-    cout << "Yesterday was: " << previousDay << endl;
+dayType::dayType(string d) {
+    setDay(d); // Use setDay function to ensure validity of input
 }
--- main.cpp
+++ main.cpp
@@ -1,47 +1,44 @@
+// Main.cpp
+
 #include <iostream>
-#include<fstream>
-#include <iomanip>
-#include "dayType.h" 
+#include "dayType.h"
 
 using namespace std;
 
-// Main function 
-// Test class 
-int main()
-{
+int main() {
+    // Test default constructor
+    dayType defaultDay;
+    cout << "From default constructor: ";
+    defaultDay.print();
+    cout << endl;
 
-	dayType day;
+    // Test nextDay() method
+    cout << "Next day: " << defaultDay.nextDay() << endl;
 
+    // Test prevDay() method
+    cout << "Previous day: " << defaultDay.prevDay() << endl;
 
-	day.printTo();
+    // Test constructor with parameters
+    dayType customDay("Monday");
+    cout << "From constructor with parameters: ";
+    customDay.print();
+    cout << endl;
 
-	string inputDay = "Monday";
+    // Test addDay() method
+    customDay.addDay(3);
+    cout << "After adding 3 days: ";
+    customDay.print();
+    cout << endl;
 
-	dayType daytwo(inputDay);
-	daytwo.printTo();
+    customDay.addDay(30);
+    cout << "After adding 30 days: ";
+    customDay.print();
+    cout << endl;
 
+    customDay.addDay(365);
+    cout << "After adding 365 days: ";
+    customDay.print();
+    cout << endl;
 
-	// Add 3 days to current day 
-	day.printTo();
-	int add1 = 3;
-	string out1;
-	out1 = day.addToDay(inputDay, add1);
-	cout << "After adding 3 days:" << out1 << endl;
-
-	int add2 = 30;
-	string out2;
-
-	// Add 30 days to current day 
-	out2 = day.addToDay(inputDay, add2);
-	cout << "After adding 30 days:" << out2 << endl;
-	int add3 = 365;
-	string out3;
-
-	// Add 365 days to current day 
-	out3 = day.addToDay(inputDay, add3);
-	cout << "After adding 365 days:" << out3 << endl;
-
-
-
-
+    return 0;
 }
