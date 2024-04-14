f23419093f8d88507b26e2979ba09ce464ade366
las13816 <las13816@email.vccs.edu>
2024-04-13 22:40:27 -0400

    Updated dayType files under dateType branch

--- dayType.h
+++ dayType.h
@@ -1,62 +1,26 @@
-#include <iostream> 
+#ifndef dayType_H
+#define dayType_H
 #include <string>
 
 using namespace std;
 
-// Class 
-class dayType
-{
+// Define class 
+class dayType {
 public:
-	// constructor
-	dayType();
+    static string weekDays[7];
+    void print() const;
+    string nextDay() const;
+    string prevDay() const;
+    void addDay(int nDays);
+    void setDay(string d);
+    string getDay() const;
 
-	// constructor with parameter. 
-	dayType(string inputDay);
-
-	// Destructor 
-	~dayType();
-
-	// Pre-condition: inputDay must be a valid day. 
-	// Post-condition: day is set. 
-	string setDay(string inputDay);
-	
-	// Pre-condition: must recieve a valid day.
-	// Post-condition: returns the current day. 
-	string returnDay();
-
-	// Prints today. 
-	void printDay(string today);
-
-	// Pre-condition: today must be a valid day. 
-	// Post-condition: returns the value for tomorrow. 
-	string setReturnNext(string today);
-
-	// Pre-condition: today must be a valid day. 
-	// Post-condition: returns the value for yesterday. 
-	string setReturnPrevious(string today);
-
-	void printTo();
-
-	// Pre-condition: today must be a day of the week.
-	// Post-condition: Returns the day after adding 'addto' days to today.
-	string addToDay(string today, int addto);
-
-
+    dayType();
+    dayType(string d);
 
 private:
-
-	// Current day 
-	string today;
-	// Array to store days of the week. 
-	string days[7];
-	// Next day 
-	string tomorrow;
-	// Yesterday 
-	string previousDay;
-	// Day after addings x number of days 
-	string addedDay;
-
-
-
-
+    string day; // Only one instance variable needed ( got rid of other instance variables ) 
 };
+
+#endif
+
